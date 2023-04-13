22a28b6f46708a792f0d53674e4734133a0cfb3a
Ryan Fleharty <ryan.fleharty@generalassemb.ly>
2023-04-13 13:07:00 -0600

    FULL STACK

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,28 +2,31 @@ const Recipe = require('../models/recipe')
 // 7 RESTful ROUTES
 // 4 OPERATIONS CRUD
 module.exports = {
-    index: (req, res)=>{
+    // async/await
+    index: async (req, res)=>{
+        const recipes = await Recipe.find()
         // We render the index template
         // We provide a CONTEXT object containing the recipes data
         res.render('recipes/index', {
             // The recipe model has our data
             // We use its methods to access the data
-            recipes: recipe.getAll()
+            recipes: recipes
         })
+
     },
     new: (req, res)=>{
         res.render('recipes/new')
     },
-    create: (req, res)=>{
+    create: async (req, res)=>{
         // Pull in the form data
         const recipeToCreate = req.body;
         // Use the model to create based on the form data
-        recipe.create(recipeToCreate);
+        const newRecipe = await Recipe.create(recipeToCreate);
         // Send the user back to a different page
         res.redirect('/recipes');
     },
-    delete: (req, res)=>{
-        recipe.deleteById(req.params.id)
+    delete: async (req, res)=>{
+        await Recipe.findByIdAndDelete(req.params.id)
         res.redirect('/recipes');
     }
 }
